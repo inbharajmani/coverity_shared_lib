5940ba20ff3e07a56c47144b7128d9642226492a
Inbharaj <inbharaj.mani@mercedes-benz.com>
2023-04-20 11:47:20 +0530

    test

--- vars/cov-configure.groovy
+++
@@ -1,4 +0,0 @@
-def call(String compiler, String config_xml_path, List skip_files) {
-    exec("echo 'test'")
-//   exec (cov-configure --compiler gcc --config config_xml_path --xml-option=skip_file:)
-}
